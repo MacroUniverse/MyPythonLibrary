eeb807a83b68c94d95d29dc46c1c545dfc429f46
MacroUniverse <270174408@qq.com>
2023-06-30 06:29:58 -0500

    Update clean_ts.py

--- clean_ts.py
+++ clean_ts.py
@@ -7,10 +7,10 @@ def delete_ts_files():
     current_dir = os.getcwd()
     for root, dirs, files in os.walk(current_dir):
         for file in files:
-            if file.endswith('.mkv'):
-                print(file)
+            if file.endswith('.ts'):
                 ts_file_path = os.path.join(root, file)
-                mp4_file_path = os.path.join(root, file[:-4] + '.mp4')
+                print(ts_file_path)
+                mp4_file_path = os.path.join(root, file[:-3] + '.mp4')
                 print(mp4_file_path)
                 if os.path.exists(mp4_file_path):
                     os.remove(ts_file_path)
