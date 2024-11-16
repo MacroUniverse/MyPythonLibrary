b2b38b204ad72460ef5e0d42c2d9c5e7f19ad5f6
MacroUniverse <270174408@qq.com>
2024-11-16 18:33:27 +0800

    Update vid_compress_batch.py

--- vid_compress_batch.py
+++ vid_compress_batch.py
@@ -11,8 +11,9 @@ def compress_file(full_path):
 	"""Compress a file to a certain bitrate using ffmpeg."""
 	directory, filename = os.path.split(full_path)
 	full_path_out = os.path.join(directory, 'ffzip_' + filename)
-	cmd = 'ffmpeg -y -i "{}" -b:v 500k "{}"'.format(full_path, full_path_out)
-	subprocess.run(cmd, shell=True)
+	cmd = f'ffmpeg -y -i "{full_path}" -max_muxing_queue_size 10000 -b:v 500k "{full_path_out}" > /dev/null 2>&1'
+	res = subprocess.run(cmd, shell=True)
+	return res.returncode, full_path_out
 
 def scan_files(directory):
 	if not os.path.exists(directory):
@@ -27,13 +28,19 @@ def scan_files(directory):
 		for filename in filenames:
 			if (filename.startswith('RPReplay') or filename.startswith('Screen_Recording')) and (filename.endswith('.mp4') or filename.endswith('.mov') or filename.endswith('.MP4') or filename.endswith('.MOV')):
 				full_path = os.path.join(foldername, filename)
-				out_path = './recycle/' + filename
 				print('\n\n', full_path)
 				print('-----------------------------------------------------------')
-				compress_file(full_path)
-				print(out_path)
-				if os.path.exists(out_path):
-					os.remove(out_path)
+				exit_code, full_path_out = compress_file(full_path)
+				if exit_code != 0:
+					print('failed!')
+					try:
+						os.remove(full_path_out)
+					except:
+						pass
+					continue
+				out_path = './recycle/' + filename
+				while os.path.exists(out_path):
+					out_path = '1' + out_path
 				shutil.move(full_path, out_path)
 	
 	# delete `./recycle` if it's empty
